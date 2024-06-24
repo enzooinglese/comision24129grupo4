98172265a5eaa321a45c78b7d3fb5777fbd5c929
marfe11984 <marfe1984@hotmail.com>
2024-06-24 16:09:25 -0300

    Adición de campo DNI

--- js/validForm.js
+++ js/validForm.js
@@ -1,17 +1,19 @@
 const validNombre =/^[a-zA-ZÀ-ÿ\s]+$/;              
 const validMail =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
-const validAlim =/^[a-zA-ZÀ-ÿ\s]+$/;
 const validMusica =/^[a-zA-ZÀ-ÿ\s]+$/; 
-                                              
+const validDocumento=/^[0-9]{7,8}$/;  
+
 const nombre = document.getElementById('nombre');   
 const confPresente = document.getElementById('presente');
 const confAusente = document.getElementById('ausente'); 
 const mail = document.getElementById('mail');
+const documento = document.getElementById('dni'); 
 
 
 const error1 = document.getElementById('error1');    
 const error2 = document.getElementById('error2');     
 const error3 = document.getElementById('error3');
+const error4 = document.getElementById('error4');
 
 
 //FUNCION PARA LA VALIDACION DEL FORMULARIO
@@ -21,6 +23,7 @@ const error3 = document.getElementById('error3');
         let mensajesError1 = [];      
         let mensajesError2 = [];
         let mensajesError3 = [];
+        let mensajesError4 = [];
 
         if(!validNombre.test(nombre.value)){          
             evt.preventDefault()                             
@@ -34,10 +37,15 @@ const error3 = document.getElementById('error3');
             evt.preventDefault()
             mensajesError3.push("El mail no es válido");      
         }
+        if(!validDocumento.test(dni.value)){
+            evt.preventDefault()
+            mensajesError4.push("El dni no es válido");      
+        }
        
         error1.innerHTML= mensajesError1.join("");   
         error2.innerHTML= mensajesError2.join("");   
         error3.innerHTML= mensajesError3.join(""); 
+        error4.innerHTML= mensajesError4.join(""); 
     }
 
     const BTNENVIAR = document.getElementById('formulario');
